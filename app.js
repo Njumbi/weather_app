f215bd2145169c247d4f901dc89160393ad444c3
toni <wambuinjumbi@gmail.com>
2020-09-29 15:14:58 +0300

    Added a controller to request weather data

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+require('dotenv').config()
+const axios = require('axios')
+const {
+    report
+} = require('process')
+const bodyParser = require('body-parser')
+const path = require('path')
+
+const app = express()
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json())
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res, next) => {
+    res.render('index', {
+        data: null
+    })
+})
+
+app.post('/', (req, res, next) => {
+    const cityName = req.body.city
+    console.log(req.body)
+    axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${OPEN_KEY}&units=metric`)
+        .then(response => {
+            console.log(response.data)
+            res.render('index', {
+                data: response.data
+            })
+        })
+        .catch(e => {
+            console.log(e)
+        })
+})
+app.listen(4000, () => {
+    console.log("app started")
+})
